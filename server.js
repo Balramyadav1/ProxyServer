ecafbed2063ed97e07c5131b6a480754b233e762
Balramyadav1 <102531225+Balramyadav1@users.noreply.github.com>
2023-06-23 00:43:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Bodyparser
+// app.set('/css',express.static(__dirname+'public/main.css'))
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.port || 5000;
+
+// Routes 
+app.use('/', require('./routes/index'));
+// app.use('/users', require('./routes/users'));
+
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+// async function run() {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const page = await browser.newPage();
+  
+//   const proxy = 'https://proxybot.io/api/v1/AQTvV70KEX04CNkqkfTIoeHYm853?url=http://httpbin.org/ip';
+//   const pageUrl = 'https://whatismyipaddress.com/';
+  
+
+//   await page.goto(pageUrl);
+// }
+
+// run();
